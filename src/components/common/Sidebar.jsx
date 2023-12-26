92d387c054e1542c7418d4348049e1d04efa8c46
DWK3732 <101383056+DWK3732@users.noreply.github.com>
2023-12-26 16:17:32 +0900

    [UI] 토글 버튼을 컨테이너 내부로 이동

--- src/components/common/Sidebar.jsx
+++ src/components/common/Sidebar.jsx
@@ -2,33 +2,7 @@ import React, { useState,useEffect } from 'react';
 import styled from 'styled-components';
 import SidebarContainer from '@components/common/containers/SidebarContainer';
 
-const ToggleButton = styled.button`
-  position: fixed;  // 고정 위치
-  top: 50%;  // 중앙에 위치
-  transform: translateY(-50%);  // 정확한 중앙 정렬을 위해
-  left: ${({ isVisible }) => isVisible ? '202px' : '0px'};  // isVisible에 따라 위치 조정
-  z-index: 10;
-  background-color: #transparent;  // 배경색
-  border: none;  // 테두리 없앰
-  border-radius: 5px;  // 둥근 모서리
-  padding: 10px;  // 패딩
-  cursor: pointer;  // 커서 스타일 변경
-  font-size: 30px;  // 폰트 크기
-  transition: 0.2s;  // 부드러운 애니메이션을 위해
-  font-weight: normal;  // 폰트 굵기
-  &:focus {
-    outline: none;  // 포커스시 테두리 없앰
-  }
-  &:hover {
-    background-color: #e0e0e0;  // 호버 시 배경색 변경
-    color: #4DBDE5;  // 호버 시 글자색 변경
-    font-weight: bold; // 호버 시 폰트 굵기 변경
-    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);  // 호버 시 그림자 효과 추가
-  }
-`;
-
 export default function Sidebar() {
-  const [isVisible, setIsVisible] = useState(true);
   const [userRole, setUserRole] = useState(null);
   const [selectedSystemId, setSelectedSystemId] = useState(null);
 
@@ -47,12 +21,7 @@ export default function Sidebar() {
 
   return (
     <>
-      {userRole === 'ADMIN' && (
-        <ToggleButton isVisible={isVisible} onClick={() => setIsVisible(!isVisible)}>
-          {isVisible ? '<' : '>'}
-        </ToggleButton>
-      )}
-      {isVisible && <SidebarContainer />}
+      {userRole === 'ADMIN' && <SidebarContainer />}
     </>
   );
 }
