3818ca2b4e8bc9303a96c667d56cd313c3a3b510
DWK3732 <101383056+DWK3732@users.noreply.github.com>
2023-12-28 15:12:36 +0900

    [Feat] 페이지 중앙에 로고를 넣는 모듈 추가

---
+++ src/components/common/MainLogo.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styled from "styled-components";
+import logo from "/assets/images/logo.png";
+
+const LogoContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 60px; // Adjust as needed
+    margin-bottom: 60px;
+`;
+
+const LogoImage = styled.img`
+    max-width: 100%;
+    height: auto;
+`;
+
+const MainLogo = () => {
+    return (
+        <LogoContainer>
+            <LogoImage src={logo} alt="Logo" />
+        </LogoContainer>
+    );
+};
+
+export default MainLogo;
