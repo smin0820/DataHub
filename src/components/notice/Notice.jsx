af21a13d926618fb063fba93a96f98207c3f9c7e
DWK3732 <101383056+DWK3732@users.noreply.github.com>
2023-12-28 17:03:17 +0900

    [Feat] 공지사항 렌더링 초기 모듈 추가

--- src/components/notice/Notice.jsx
+++ src/components/notice/Notice.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import AdminHeaderContainer from '@components/admin/containers/AdminHeaderContainer'
+import Sidebar from '@components/common/Sidebar';
+import MainLogo from '@components/common/MainLogo';
+import NoticeTableContainer from '@components/notice/containers/NoticeTableContainer';
 
 export default function Notice() {
     return (
         <>
             <AdminHeaderContainer></AdminHeaderContainer>
+            <Sidebar></Sidebar>
+            <MainLogo></MainLogo>
+            <NoticeTableContainer></NoticeTableContainer>
         </>
     );
 }
