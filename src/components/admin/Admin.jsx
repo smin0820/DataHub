93e7714af3dc83ecee8eedf8431c76e09648c1a7
DWK3732 <101383056+DWK3732@users.noreply.github.com>
2023-12-28 15:13:02 +0900

    [UI] 대기중 게시물 페이지에 로고 추가

--- src/components/admin/Admin.jsx
+++ src/components/admin/Admin.jsx
@@ -3,6 +3,7 @@ import AdminHeaderContainer from '@components/admin/containers/AdminHeaderContai
 import AdminSearchContainer from '@components/admin/containers/AdminSearchContainer';
 import Sidebar from '@components/common/Sidebar';
 import AdminContentContainer from './containers/AdminContentContainer';
+import MainLogo from '@components/common/MainLogo';
 
 
 
@@ -13,6 +14,7 @@ export default function Admin() {
         <>
             <AdminHeaderContainer></AdminHeaderContainer>
             <Sidebar></Sidebar>
+            <MainLogo></MainLogo>
             <AdminSearchContainer></AdminSearchContainer>
             <AdminContentContainer></AdminContentContainer>
         </>
