4e8955eb24a1b472a8dd68088f4633eac53a6160
smin0820 <97029953+smin0820@users.noreply.github.com>
2023-12-28 13:33:10 +0900

    [UI} 네브바에 공지사항, Q&A 추가

---
+++ src/components/notice/Notice.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import AdminHeaderContainer from '@components/admin/containers/AdminHeaderContainer'
+
+export default function Notice() {
+    return (
+        <>
+            <AdminHeaderContainer></AdminHeaderContainer>
+        </>
+    );
+}
+
