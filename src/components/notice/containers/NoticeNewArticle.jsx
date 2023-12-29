d5e40920d40676f1f8e7709b3c6dee4f0fa745b0
DWK3732 <101383056+DWK3732@users.noreply.github.com>
2023-12-29 13:13:23 +0900

    [Feat] 게시글 신규 등록 버튼 추가

---
+++ src/components/notice/containers/NoticeNewArticle.jsx
@@ -0,0 +1,42 @@
+// NoticeNewArticle.jsx
+// 새로운 공지사항을 등록하기 위한 버튼을 렌더링하는 컨테이너 컴포넌트
+
+import React from 'react';
+import styled from 'styled-components';
+import useModal from '@hooks/useModal';
+import NoticeRegisterModal from '@components/notice/modals/NoticeReigsterModal';
+
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    div {
+        width: 100%;
+        max-width: 1000px;
+    
+        button {
+            padding: 5px 10px;
+            color: white;
+            background-color: #007FFF;
+            border: 1px solid #007FFF;
+            border-radius: 10px;
+            font-size: medium;
+            cursor: pointer;
+            float: right;
+        }
+    }
+`;
+
+export default function NoticeNewArticle() {
+    const { isOpen, openModal, closeModal } = useModal();
+
+    return (
+        <Container>
+            <div>
+                <button onClick={openModal}>
+                    공지사항 등록
+                </button>
+            </div>
+            {isOpen && <NoticeRegisterModal closeModal={closeModal} />}
+        </Container>
+    )
+};
