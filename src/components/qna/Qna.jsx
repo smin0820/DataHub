cebef78983066695a65de4d866eec47b66378c28
smin0820 <97029953+smin0820@users.noreply.github.com>
2023-12-29 17:04:37 +0900

    [UI] Q&A 페이지 글쓰기 버튼, 모달 추가

--- src/components/qna/Qna.jsx
+++ src/components/qna/Qna.jsx
@@ -3,12 +3,14 @@ import AdminHeaderContainer from '@components/admin/containers/AdminHeaderContai
 import Sidebar from '@components/common/Sidebar';
 import MainLogo from '@components/common/MainLogo';
 import QnaTableContainer from './containers/QnaTableContainer';
+import NoticeNewArticle from './containers/QnaNewArticle';
 export default function Qna() {
     return (
         <>
             <AdminHeaderContainer></AdminHeaderContainer>
             <Sidebar></Sidebar>
             <MainLogo></MainLogo>
+            <NoticeNewArticle></NoticeNewArticle>
             <QnaTableContainer></QnaTableContainer>
         </>
     );
