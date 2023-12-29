34454c7ff0c7ce715eea870e3d2301e5726f9bfb
DWK3732 <101383056+DWK3732@users.noreply.github.com>
2023-12-29 14:29:28 +0900

    [UI] 공지사항 드롭다운 메뉴 UI 수정

--- src/components/notice/containers/NoticeDropdown.jsx
+++ src/components/notice/containers/NoticeDropdown.jsx
@@ -15,23 +15,27 @@ const Dropmenu = styled.div`
     width: 80px;
     border: 1px solid #E5EAF2;
     border-radius: 10px;
-
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);  /* 그림자 효과 추가 */
     background-color: white;
 
     ul {
         list-style: none;
         display: flex;
         flex-direction: column;
-        gap: 5px;
         padding-right: 20px;
         margin: 0;
         padding: 0;
     }
     li {
         cursor: pointer;
-                padding: 4px 12px;
+        padding: 4px 12px;
         border-bottom: 1px solid #E5EAF2; /* 항목 사이에 구분선을 추가 */
-        
+        &:hover {
+            background-color: #e0e0e0;  // 호버 시 배경색 변경
+            color: #4DBDE5;  // 호버 시 글자색 변경
+            font-weight: bold; // 호버 시 폰트 굵기 변경
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);  // 호버 시 그림자 효과 추가
+        }
         &:last-child {
             border-bottom: none; /* 마지막 항목에는 구분선을 제거 */
         }
