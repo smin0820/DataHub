4a37cd629399c2ac7a18eb0313fc995d4060c39e
DWK3732 <101383056+DWK3732@users.noreply.github.com>
2023-12-29 13:14:44 +0900

    [UI] 편집 드롭다운 메뉴 UI 수정

--- src/components/notice/containers/NoticeDropdown.jsx
+++ src/components/notice/containers/NoticeDropdown.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+import useModal from '@hooks/useModal';
+import NoticeEditModal from '@components/notice/modals/NoticeEditModal';
+import NoticeDeleteModal from '@components/notice/modals/NoticeDeleteModal';
 
 const Dropmenu = styled.div`
     display: flex;
@@ -9,23 +12,29 @@ const Dropmenu = styled.div`
     z-index: 20;
     top: 100%;
     right: 0px;
-    width: 65px;
-    border: 2px solid #E5EAF2;
-    border-radius: 15px;
-    padding: 10px 15px;
-    padding-right: 50px;
+    width: 80px;
+    border: 1px solid #E5EAF2;
+    border-radius: 10px;
+
     background-color: white;
 
     ul {
         list-style: none;
         display: flex;
         flex-direction: column;
-        gap: 7px;
+        gap: 5px;
+        padding-right: 20px;
         margin: 0;
         padding: 0;
     }
     li {
         cursor: pointer;
+                padding: 4px 12px;
+        border-bottom: 1px solid #E5EAF2; /* 항목 사이에 구분선을 추가 */
+        
+        &:last-child {
+            border-bottom: none; /* 마지막 항목에는 구분선을 제거 */
+        }
     }
     &.active {
         color: #4DBDE5;
@@ -33,13 +42,19 @@ const Dropmenu = styled.div`
 `
 
 export default function NoticeDropdown() {
+    const { isOpen: isEditOpen, openModal: openEditModal, closeModal: closeEditModal } = useModal();
+    const { isOpen: isDeleteOpen, openModal: openDeleteModal, closeModal: closeDeleteModal } = useModal();
 
     return (
-        <Dropmenu>
-            <ul>
-                <li >정보수정</li>
-                <li >로그아웃</li>
-            </ul>
-        </Dropmenu>
+        <>
+            <Dropmenu>
+                <ul>
+                    <li onClick={openEditModal}>수정</li>
+                    <li onClick={openDeleteModal}>삭제</li>
+                </ul>
+            </Dropmenu>
+            {isEditOpen && <NoticeEditModal closeModal={closeEditModal} />}
+            {isDeleteOpen && <NoticeDeleteModal closeModal={closeDeleteModal} />} 
+        </>
     );
 }
