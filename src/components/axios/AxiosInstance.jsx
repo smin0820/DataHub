c929e52e021270172eeee4d56222bd1eb135bd3b
DWK3732 <101383056+DWK3732@users.noreply.github.com>
2024-01-03 16:22:28 +0900

    [Test] 클라우드 연동 테스트

--- src/components/axios/AxiosInstance.jsx
+++ src/components/axios/AxiosInstance.jsx
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: "http://54.166.160.145:8080",
-
+    //baseURL: "http://54.166.160.145:8080",
+    //baseURL: "http://localhost:8080",
+    baseURL: "http://43.203.63.39:8080",
+    //baseURL: ""
 });
 
 export default axiosInstance;
