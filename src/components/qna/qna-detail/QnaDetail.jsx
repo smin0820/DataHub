5643e3487c418176f164e3e6f8de0da10896bb63
smin0820 <97029953+smin0820@users.noreply.github.com>
2024-01-12 13:07:30 +0900

    [Refactor] QnaDetail 폴더 위치 변경 및 Comment 컴포넌트 이름 변경

--- src/components/qna/qna-detail/QnaDetail.jsx
+++ src/components/qna/qna-detail/QnaDetail.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import HeaderContainer from '@components/common/Header/HeaderContainer'
 import QnaDetailTitle from './containers/QnaDetailTitle';
-import Comments from './containers/Comments';
+import QnaDetailComments from './containers/QnaDetailComments';
 
 export default function QnaDetail() {
     return (
         <>
             <HeaderContainer></HeaderContainer>
             <QnaDetailTitle></QnaDetailTitle>
-            <Comments></Comments>
+            <QnaDetailComments></QnaDetailComments>
         </>
     );
 }
