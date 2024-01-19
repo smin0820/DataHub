31b634cbcda3ff4babfa5ecfba43c7770f5c0e5c
smin0820 <97029953+smin0820@users.noreply.github.com>
2024-01-19 17:37:06 +0900

    [Refactor] 대기중 게시물 CSS 모듈화

--- src/components/admin/AdminTable/styles.ts
+++ src/components/admin/AdminTable/styles.ts
@@ -1,54 +1,23 @@
 import styled from 'styled-components';
 import * as S from '@styles/BoardStyles';
 
-export const Boarddiv = styled.div`
-    display: flex;
-    justify-content: center;
-    margin-bottom: 30px;
-    table {
-        width: 100%;
-        max-width: 1000px;
-        border-collapse: collapse;
-        border-spacing: 0;
-        caption {
-            text-align: left;
-            margin-bottom: 30px;
-            font-weight: bolder;
-        }
-    }
-    thead {
-        background-color: #f3f6f9;
-    }
-    th {
-        width: auto;
-        text-align: left;
-        padding: 7px 5px;
-        font-weight: normal;
-    }
-    td {
-        width:auto;
-        padding: 7px 5px;
-    }
-    td:last-child {
-        display: flex;
-        justify-content: flex-end;
-        align-items: center;
-    }
+export const Boarddiv = styled(S.Boarddiv)`
     button {
-            padding: 2px 5px;
-            color: white;
-            background-color: #007fff;
-            border: 1px solid #007fff;
-            border-radius: 10px;
-            font-size: medium;
-            cursor: pointer;
-            text-align: center;
+        padding: 2px 5px;
+        color: white;
+        background-color: #007fff;
+        border: 1px solid #007fff;
+        border-radius: 10px;
+        font-size: medium;
+        cursor: pointer;
+        text-align: center;
     }
-    button.delete { // 삭제 버튼
-        background-color: #CCCCCC;
-        border: 1px solid #CCCCCC;
+    button.delete {
+        // 삭제 버튼
+        background-color: #cccccc;
+        border: 1px solid #cccccc;
     }
-`
+`;
 
 export const StyledLink = styled(S.StyledLink)``
 
